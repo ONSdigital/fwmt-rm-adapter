0ece9d181fc74447f346b7e708c6829d0e314665
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2018-08-03 12:53:07 +0100

    Added base spring boot project

---
+++ src/main/java/uk/gov/ons/fwmt/fwmtrmadapter/FwmtRmAdapterApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.ons.fwmt.fwmtrmadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwmtRmAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwmtRmAdapterApplication.class, args);
+	}
+}
