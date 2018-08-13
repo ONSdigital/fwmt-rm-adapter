bc3c9e38a4c3ba3fece0ed3cd9cbc17b35e06001
jacobharrison <jacob.harrison@qa.com>
2018-08-13 15:13:11 +0100

    Generalised sendMessage to take all messages types.

--- src/main/java/uk/gov/ons/fwmt/fwmtrmadapter/message/impl/JobServiceProducerImpl.java
+++ src/main/java/uk/gov/ons/fwmt/fwmtrmadapter/message/impl/JobServiceProducerImpl.java
@@ -4,43 +4,34 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Exchange;
-import org.springframework.beans.factory.annotation.Qualifier;
-import uk.gov.ons.fwmt.fwmtrmadapter.message.JobServiceProducer;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.fwmt.fwmtgatewaycommon.FWMTCreateJobRequest;
+import uk.gov.ons.fwmt.fwmtrmadapter.message.JobServiceProducer;
 
 @Slf4j
 @Component
 public class JobServiceProducerImpl implements JobServiceProducer {
 
-  @Autowired
-  private RabbitTemplate rabbitTemplate;
+  @Autowired private RabbitTemplate rabbitTemplate;
+  @Autowired @Qualifier("adapterToJobSvcQueue") private Queue queue;
+  @Autowired private Exchange exchange;
 
-  @Autowired
-  @Qualifier("adapterToJobSvcQueue")
-  private Queue queue;
-
-  @Autowired
-  private Exchange exchange;
-
-  public void sendCreateJobRequest(FWMTCreateJobRequest createJobRequest) {
+  public <T> void sendMessage(Object dto) {
     try {
-      String JSONJobRequest = convertToJSON(createJobRequest);
+      String JSONJobRequest = convertToJSON(dto);
       rabbitTemplate.convertAndSend(exchange.getName(), "job.svc.job.request.create", JSONJobRequest);
     } catch(JsonProcessingException e) {
       e.printStackTrace();
     }
-    log.info("Done!");
-
+    log.info("Message send to queue", dto);
   }
 
-  private String convertToJSON(FWMTCreateJobRequest createJobRequest) throws JsonProcessingException {
-
+  private <T> String convertToJSON(Object dto) throws JsonProcessingException {
     ObjectMapper mapper = new ObjectMapper();
-    String JSONJobRequest = mapper.writeValueAsString(createJobRequest);
+    String JSONJobRequest = mapper.writeValueAsString(dto);
     log.info(JSONJobRequest);
     return JSONJobRequest;
   }
--- src/main/java/uk/gov/ons/fwmt/fwmtrmadapter/service/impl/RMAdapterServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/fwmtrmadapter/service/impl/RMAdapterServiceImpl.java
@@ -1,58 +1,35 @@
 package uk.gov.ons.fwmt.fwmtrmadapter.service.impl;
 
-import org.springframework.stereotype.Component;
-import uk.gov.ons.fwmt.fwmtgatewaycommon.DummyTMResponse;
-import uk.gov.ons.fwmt.fwmtrmadapter.message.JobServiceProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import uk.gov.ons.fwmt.fwmtrmadapter.message.RMProducer;
-import uk.gov.ons.fwmt.fwmtrmadapter.service.RMAdapterService;
-import uk.gov.ons.ctp.response.action.message.instruction.ActionAddress;
+import org.springframework.stereotype.Component;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionInstruction;
-import uk.gov.ons.ctp.response.action.message.instruction.ActionRequest;
-import uk.gov.ons.fwmt.fwmtgatewaycommon.Address;
-import uk.gov.ons.fwmt.fwmtgatewaycommon.FWMTCreateJobRequest;
+import uk.gov.ons.fwmt.fwmtgatewaycommon.DummyTMResponse;
 import uk.gov.ons.fwmt.fwmtrmadapter.data.DummyRMReturn;
+import uk.gov.ons.fwmt.fwmtrmadapter.message.RMProducer;
+import uk.gov.ons.fwmt.fwmtrmadapter.message.impl.JobServiceProducerImpl;
+import uk.gov.ons.fwmt.fwmtrmadapter.service.RMAdapterService;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+@Slf4j
 @Component
 public class RMAdapterServiceImpl implements RMAdapterService {
 
-  @Autowired
-  JobServiceProducer jobServiceProducer;
+  @Autowired JobServiceProducerImpl jobServiceProducer;
+  @Autowired MessageConverterImpl messageConverter;
+  @Autowired RMProducer rmProducer;
 
-  @Autowired
-  RMProducer rmProducer;
 
   public void sendJobRequest(ActionInstruction actionInstruction) {
-    FWMTCreateJobRequest createJobRequest = transformActionInstruction(actionInstruction);
-    jobServiceProducer.sendCreateJobRequest(createJobRequest);
-  }
+    if (actionInstruction.getActionRequest().getActionType().matches("create")) {
+      jobServiceProducer.sendMessage(messageConverter.createJob(actionInstruction));
+    }
+    if (actionInstruction.getActionRequest().getActionType().matches("update")) {
+      //TODO add code for update
 
-  private FWMTCreateJobRequest transformActionInstruction(ActionInstruction actionInstruction) {
-    FWMTCreateJobRequest createJobRequest = new FWMTCreateJobRequest();
-    ActionRequest actionRequest = actionInstruction.getActionRequest();
-    ActionAddress actionAddress = actionRequest.getAddress();
-
-    Address address = new Address();
-    address.setLatitude(actionAddress.getLatitude());
-    address.setLongitude(actionAddress.getLongitude());
-    address.setLine1(actionAddress.getLine1());
-    address.setLine2(actionAddress.getLine2());
-    address.setLine3(actionAddress.getLine3());
-    address.setLine4(actionAddress.getLine4());
-    address.setPostCode(actionAddress.getPostcode());
-    address.setTownName(actionAddress.getTownName());
-
-    createJobRequest.setJobIdentity(actionRequest.getCaseId());
-    createJobRequest.setSurveyType(actionRequest.getSurveyRef());
-    //createJobRequest.setMandatoryResourceAuthNo(actionRequest();
-    //createJobRequest.setPreallocatedJob();
-    createJobRequest.setDueDate(LocalDate.parse(actionRequest.getReturnByDate(), DateTimeFormatter.BASIC_ISO_DATE));
-    createJobRequest.setAddress(address);
-
-    return createJobRequest;
+    }
+    if (actionInstruction.getActionRequest().getActionType().matches("cancel")) {
+      jobServiceProducer.sendMessage(messageConverter.cancelJob(actionInstruction));
+    }
   }
 
   public void returnJobRequest(DummyTMResponse response) {
@@ -66,5 +43,4 @@ public class RMAdapterServiceImpl implements RMAdapterService {
     rmReturnMessage.setIdentity(response.getIdentity());
     return rmReturnMessage;
   }
-
 }
